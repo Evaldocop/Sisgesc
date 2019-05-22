8cffd6088cb5634d8f2637aa8ac6c5032cb89de0
303090293 <evaldo@pm.ba.gov.br>
2019-05-22 12:28:45 -0300

    testando github

--- sisgesc/src/main/java/ettec/com/sisgesc/business/BookmarkBC.java
+++ sisgesc/src/main/java/ettec/com/sisgesc/business/BookmarkBC.java
@@ -13,6 +13,9 @@ public class BookmarkBC extends DelegateCrud<Bookmark, Long, BookmarkDAO> {
 	
 	private static final long serialVersionUID = 1L;
 	
+	
+	
+	
 	@Startup
 	@Transactional
 	public void load() {
